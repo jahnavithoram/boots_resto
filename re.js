02ddde7804c7cef05dc9929bd8239d7679c8c7ab
Thoram <jthoram@teksystems.com>
2021-03-19 01:49:33 +0530

    Add project files.

---
+++ re.js
@@ -0,0 +1,21 @@
+
+    function openForm() {
+        document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+        document.getElementById("myForm").style.display = "none";
+}
+function clickAlert() {
+    alert("Booking Confirm!");
+}
+function openForm2() {
+    document.getElementById("myForm2").style.display = "block";
+}
+
+function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+}
+function clickAlert2() {
+    alert("Message received");
+}
